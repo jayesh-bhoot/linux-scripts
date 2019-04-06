2a32922989e61aa0528a10b3c84b01d6a8094da7
Jayesh Bhoot <jayesh@bhoot.sh>
2019-04-06 22:33:10 +0530

    Update wallpaper's dir name

--- wallpaper-slideshow.sh
+++ wallpaper-slideshow.sh
@@ -2,7 +2,7 @@
 
 cd /tmp
 while true; do
-	find /mnt/windows/data2/wallpapers/asoiaf -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
+	find ~/wallpapers -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |
 		shuf -n1 -z | xargs -0 feh --bg-scale
 	sleep 3m
 done
